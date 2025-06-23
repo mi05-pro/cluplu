945d0ce57805b7782cfef2740d5b5dd6a077a3c2
mi05-pro <mici050403@gmail.com>
2025-06-23 14:27:10 +0200

    hw3: long double, double, float

---
+++ hw3/task3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  unsigned int age;  // >0
+  double backyard_area;  // m2
+  unsigned long int stars_in_galaxy;  // >10000000000
+  double avg_precipitation_january = 34.5;  // mm
+  const double pi = 3.14159; // long double, double, float
+  float copypi = pi;
+  //
+  cout << copypi;
+  return 0;
+}
