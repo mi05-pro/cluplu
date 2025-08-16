92b5df522c2353c56acf6ec21ff00645d9076c5c
mi05-pro <mici050403@gmail.com>
2025-08-16 13:39:17 +0200

    20_3,20_4

---
+++ hw20_4.cpp
@@ -0,0 +1,37 @@
+//1 - using namespace std;  // Додаємо це для використання стандартного простору імен
+/*
+#include <iostream>
+
+using namespace std;  // Додаємо це для використання стандартного простору імен
+
+int main()
+{
+    cout << "Hello world!" << endl;  // Тепер все працює
+    return 0;
+}
+*/
+
+
+// 2 -    std::
+/*
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;  // Тепер все працює
+    return 0;
+}
+*/
+
+
+//3 -    using namespace std;  // Додаємо це для використання стандартного простору імен в середині фїї
+
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;  // Додаємо це для використання стандартного простору імен в середині фїї
+    cout << "Hello world!" << endl;  // Тепер все працює
+    return 0;
+}
